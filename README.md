1b3279d3ad1b9776154dfaea46568b2324684cd8
Alex Clement <aclement@kentdenver.org>
2015-08-31 12:07:28 -0600

    Project details

---
+++ README.md
@@ -0,0 +1,8 @@
+## Pull Request Demo
+
+Here you will practice the workflow of contributing to an existing project. 
+
+1. Fork this project
+2. Create your own Java class that *implements* the Student interface
+3. Create a pull request to merge your class into this repo
+
---
+++ Student.java
@@ -0,0 +1,22 @@
+public interface Student{
+
+	/**
+	 * This method provides the name stored in the class
+	 *
+	 * @return a String that is the name author of the class
+	 */
+	public String getName();
+
+	/**
+	 * This provides the food stored in the class
+	 *
+	 * @return a String that is the favorite food of the author of the class
+	 */
+	public String getFavoriteFood();
+
+	/**
+	 * Prints the catch phrase of the author to standard out
+	 *
+	 */
+	public void sayCatchphrase();
+}
