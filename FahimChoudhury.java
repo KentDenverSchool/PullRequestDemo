63cd6b91a26089c724f981473c1137a556d08ba4
Fahim Choudhury <fchoudhury17@kentdenver.org>
2015-09-01 09:34:18 -0600

    Removed Package

--- FahimChoudhury.java
+++ FahimChoudhury.java
@@ -1,16 +1,14 @@
-package Demo;
-
 public class FahimChoudhury implements Student{
 	public String getName()
 	 {
 	    return "Fahim Choudhury";
 	  }
-	
+
 	 public String getFavoriteFood()
 	  {
 	    return "Beef Tongue";
 	  }
-	
+
   public void sayCatchphrase()
 	  {
 	    System.out.println("rip");
