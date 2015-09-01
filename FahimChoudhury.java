ee09f4293c8e45aecc8784031261ab3864927a38
Fahim Choudhury <fchoudhury17@kentdenver.org>
2015-09-01 09:20:45 -0600

    First Commit

---
+++ FahimChoudhury.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class FahimChoudhury implements Student{
+	public String getName()
+	 {
+	    return "Fahim Choudhury";
+	  }
+	
+	 public String getFavoriteFood()
+	  {
+	    return "Beef Tongue";
+	  }
+	
+  public void sayCatchphrase()
+	  {
+	    System.out.println("rip");
+	  }
+}
