9d054135951a6749c91bbe85d75b486a0044edb4
russell-stewart <rstewart16@kentdenver.org>
2015-08-31 18:21:49 -0600

    Add Russell.java, implements Student interface

---
+++ Russell.java
@@ -0,0 +1,17 @@
+public class Russell implements Student
+{
+  public String getName()
+  {
+    return "Russell O. Stewart";
+  }
+
+  public String getFavoriteFood()
+  {
+    return "Barbecue and Turnip Greens";
+  }
+
+  public void sayCatchphrase()
+  {
+    System.out.println("nyoooooooom");
+  }
+}
