e26ba22308fab8687e30a6565cc8ad605093db3d
Jake Zane <jzane16@kentdenver.org>
2015-09-03 08:37:47 -0600

    Jake.java

--- Jake.java
+++ Jake.java
@@ -1,2 +1,19 @@
 public class Jake implements Student {
+    
+    String name = new String();
+    name = "Jake Zane";
+    String food = new String();
+    food = "Perogies";
+    
+    public String getName() {
+        return name;   
+    }
+    
+    public String getFavoriteFood() {
+        return food;   
+    }
+    
+    public void sayCatchphrase() {
+        System.out.println("Alright, spectacular.");   
+    }
 }
