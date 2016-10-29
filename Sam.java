35c5acd42ad1942f876a02ba6db09052bb327329
Sam <yahoosam@gmail.com>
2016-10-28 23:29:14 -0400

    Update Sam.java

--- Sam.java
+++ Sam.java
@@ -1,1 +1,18 @@
-
+public class Sam implements Student 
+{        
+    String name = new String();    
+    name = "Sam Kumar";    
+    String food = new String();    
+    food = "Pizza";        
+    public String getName() {        
+       return name;       
+    }        
+    public String getFavoriteFood() 
+    {        
+       return food;       
+    }        
+    public void sayCatchphrase() 
+    {        
+       System.out.println("Awesome");       
+    }
+}
